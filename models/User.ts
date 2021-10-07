24a4c21832ac944f9d51b5f95939f8e71a74dbe2
Codelord <macbookpro@Candace.local>
2021-10-07 07:08:48 +0100

    user auth added

---
+++ models/User.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from "mongoose";
+import { UserMongoose } from "../types/UserMongoose";
+import isEmail from "validator/lib/isEmail";
+import bcrypt from "bcrypt";
+
+export const userSchema = new Schema<UserMongoose>({
+  name: {
+    type: String,
+    required: [true, "Please enter your full name"],
+    minLength: [1, "Please enter your full name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a valid password"],
+    minLength: [6, "Minimum password length must be 6 characters"],
+  },
+  date_added: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// fire a function before a new user has been saved to the database
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = model("user", userSchema);
+
+export default User;
---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { errors } from "../../../constants/errors";
+import connectDB from "../../../middleware/mongodb";
+import User from "../../../models/User";
+import { HttpMethod } from "../../../types/HttpMethod";
+import { UserMongoose } from "../../../types/UserMongoose";
+import { handleAuthErrors } from "../../../util/apiHelpers/handleAuthErrors";
+
+const signUpAPIRoute = async (req: NextApiRequest, res: NextApiResponse) => {
+  const method = req.method as HttpMethod;
+
+  if (method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).json({
+      error: errors.REQUEST_METHOD_INVALID,
+    });
+    return;
+  }
+
+  try {
+    const body: UserMongoose = req.body;
+
+    if (!body) {
+      res.status(400).json({ error: errors.REQUEST_BODY_INVALID });
+      return;
+    }
+    const { email, password, name } = body;
+
+    const user = new User({ name, email, password });
+
+    user.save();
+    res.status(201).json({ _id: user._id });
+  } catch (err) {
+    const errors = handleAuthErrors(err);
+    res.status(400).json({ errors });
+    console.error(errors);
+  }
+};
+
+export default connectDB(signUpAPIRoute);
